23dcb00efc5eb157d0abe1972222f9fae5498b9d
Avishek-NITT <avishekmajumder2004@gmail.com>
2024-05-24 20:55:46 +0530

    feat: Added game plan

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 console.log("This is DELTA WEBDEV TASK1")
 
+
+//PROPOSED GAME LOGIC
+
+
+// -> All pieces are represented by their respective classes.
+// -> Thier instances will contain all info about them (position, direction anol)
+// -> The grid will be a grid, containing "divs" as individual cells.
+// -> the information in the game state will keep on updating the cells of the grid 
+// accordingly
+
+
+
+
+class Game {
+    //This might be the instance of the game to store the full game state.
+    //this might be useful in hacker++ in loading a saved game 
+}
+
+
+
+class Player {
+    // This will store the positions, stats and all for the each class.
+    // Two instances will be spawned for two player mode
+}
+
+
+
+
+// Piece objects
+class Titan {
+
+}
+
+class Tank {
+
+}
+
+class Ricochet {
+
+}
+
+class Semi_Ricochet {
+
+}
+
+class Canon {
+
+}
