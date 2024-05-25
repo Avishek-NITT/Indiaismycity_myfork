355c4ef0648ed36c3e4fbe86ba422018c7ee68f1
Avishek-NITT <avishekmajumder2004@gmail.com>
2024-05-25 13:07:36 +0530

    feat: Piece spawining logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("This is DELTA WEBDEV TASK1")
 const gridContainer = document.querySelector(".grid-container")
-
+const gridItems = []  //Pointer to all the individual cells
+// represented by griditem_${i}_${j}
 
 const ROW = 7
 const COLUMN = 7
@@ -31,9 +32,18 @@ class Player {
     // Two instances will be spawned for two player mode
 
     // pieces is an array which will have the instances of the pieces
+    //Order of pieces is important
     pieces = []
     
 
+    draw(){
+        this.pieces.forEach((piece) => {
+            let pos = [piece.pos[0], piece.pos[1]];
+            let item = document.getElementById(`griditem_${pos[0]}_${pos[1]}`)
+            item.innerHTML = piece.text
+        })
+
+    }
 
 }
 
@@ -42,30 +52,35 @@ class Player {
 
 // Piece objects
 class Titan {
+    pos = []
     constructor(){
         this.text = "Titan"
     }
 }
 
 class Tank {
+    pos = []
     constructor(){
         this.text = "Tank"
     }
 }
 
 class Ricochet {
+    pos = []
     constructor(){
         this.text = "Ricochet"
     }
 }
 
 class Semi_Ricochet {
+    pos = []
     constructor(){
         this.text = "Semi_Ricochet"
     }
 }
 
 class Canon {
+    pos = []
     constructor(){
         this.text = "Canon"
     }
@@ -84,36 +99,59 @@ let p1 = new Player;
 
 function fillGrid() {
     // Loop to create grid items and fill the grid
-    for (let i = 0; i < ROW * COLUMN; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridContainer.appendChild(gridItem);
+    for (let i = 0; i < ROW ; i++) {
+        let temp = []
+        for(let j = 0; j < COLUMN; j++){
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.id = `griditem_${i}_${j}`
+            gridContainer.appendChild(gridItem);
+
+            //Store it in global array
+
+            temp.push(gridItem)
+        }
+        gridItems.push(temp)
     }
 }
 
 
 function spawnPlayers() {
+
+    //Order of pieces is important
     p1.pieces.push(new Titan);
     p1.pieces.push(new Tank);
     p1.pieces.push(new Ricochet);
     p1.pieces.push(new Semi_Ricochet);
     p1.pieces.push(new Canon);
 
+
+    //Initial position of the pieces
+    p1.pieces[0].pos = [0,0];
+    p1.pieces[1].pos = [0,1];
+    p1.pieces[2].pos = [0,2];
+    p1.pieces[3].pos = [0,3];
+    p1.pieces[4].pos = [0,4];
+
+
+    
+
 }
 
 
 
 //For testing purposes
-function test(){
-    p1.pieces.forEach((instance) => {
-        console.log(instance.text)
-    })
+function draw(){
+    // gridItems.forEach((items) =>{
+    //     console.log(items)
+    // })
+    p1.draw()
 }
 
 
 window.onload = function() {
     fillGrid()
     spawnPlayers()
-    
+    draw()
 }
 
